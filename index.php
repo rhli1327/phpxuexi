276a7ea4009f175e8db831a9a798b2cfe98c05d7
rhli <rhliwow1@outlook.com>
2021-01-21 20:56:26 +0800

    commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+ <head>
+  <title>PHP 测试</title>
+ </head>
+ <body>
+ <?php echo '<p>Hello World</p>';
+        
+ ?>
+ <a href="https://bigyellowbean.com/ch3/feedback.html">Feedback</a>
+ </body>
+</html>
