518a551e0d97817028e416f4ae8525166b542752
rhli <rhliwow1@outlook.com>
2021-01-24 17:41:06 +0800

    nl2br函数加入

--- ch5/handle_post.php
+++ ch5/handle_post.php
@@ -12,12 +12,11 @@
 
 $first_name = $_POST['first_name'];
 $last_name = $_POST['last_name'];
-$posting = $_POST['posting'];
+$posting = nl2br($_POST['posting']);
 
 $name = $first_name . ' ' . $last_name;
 
-print "<div>Thank you, $name, for your posting:
-    <p>$posting</p></div>";
+print "<div>Thank you, $name, for your posting:<p>$posting</p></div>";
 
 ?>
     
