ecc8a6a64bb183748d9258efbfdaf390773e6b3b
rhli <rhliwow1@outlook.com>
2021-01-24 18:16:48 +0800

    安全性改动

--- ch5/handle_post.php
+++ ch5/handle_post.php
@@ -16,7 +16,16 @@ $posting = nl2br($_POST['posting']);
 
 $name = $first_name . ' ' . $last_name;
 
-print "<div>Thank you, $name, for your posting:<p>$posting</p></div>";
+$html_post = nl2br(htmlentities($_POST['posting']));
+$strip_post = nl2br(strip_tags($_POST['posting']));
+//两个处理php字符串变量中的HTML标签的函数
+//防止出现格式错误及XSS攻击
+//还有htmlspecialchars()可以用 处理特定的HTML标签
+print "<div>Thank you, $name, for your posting:
+    <p>Original: $posting</p></div>
+    <p>Entity: $html_post</p>
+    <p>Stripped: $strip_post</p>
+    ";
 
 ?>
     
