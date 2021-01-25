a33f798a97e87adaeb499ab1a0893d07a1107949
rhli <rhliwow1@outlook.com>
2021-01-25 17:09:56 +0800

    添加备注

--- ch5/handle_post.php
+++ ch5/handle_post.php
@@ -16,16 +16,31 @@ $posting = nl2br($_POST['posting']);
 
 $name = $first_name . ' ' . $last_name;
 
-$html_post = nl2br(htmlentities($_POST['posting']));
-$strip_post = nl2br(strip_tags($_POST['posting']));
+//$html_post = nl2br(htmlentities($_POST['posting']));
+//$strip_post = nl2br(strip_tags($_POST['posting']));
 //两个处理php字符串变量中的HTML标签的函数
 //防止出现格式错误及XSS攻击
 //还有htmlspecialchars()可以用 处理特定的HTML标签
-print "<div>Thank you, $name, for your posting:
+/*print "<div>Thank you, $name, for your posting:
     <p>Original: $posting</p></div>
     <p>Entity: $html_post</p>
     <p>Stripped: $strip_post</p>
-    ";
+    ";*/
+
+//下为urlencode版本,urlencode()函数可以通过url安全地将数据传输到php脚本
+print "<div>
+Thank you, $name, for your posting:
+    <p>$posting</p>
+</div>";
+
+//创建连接另一页面的链接
+$name = urldecode($name);
+$email = urldecode($_POST['email']);
+print "<p>Click <a href=\"thanks.php?
+name=$name&email=$email \">here</a> to continue.
+</p>";
+//传输给thanks.php
+
 
 ?>
     
