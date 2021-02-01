75cef658d49027a4c87a482a917e01dfa98b3a53
rhli-desktop <rhliwow1@outlook.com>
2021-02-01 23:08:48 +0800

    优化年份写法并添加判断是否勾选同意框

--- ch6/handle_reg.php
+++ ch6/handle_reg.php
@@ -30,8 +30,15 @@ if(empty($_POST['password']))
     $okay = false;
 }
 
+//检查密码是否相等
+if ($_POST['password'] != $_POST['confirm'])
+{
+    print '<p class = "error"> Your confirmed password doesn\'t match the original password.</p>';
+    $okay = false;    
+}
+
 //验证出生年
-if (is_numeric($_POST['year']))
+/*if (is_numeric($_POST['year']))
 {
     $age = 2021 - $_POST['year'];
 }
@@ -40,6 +47,41 @@ else
     print '<p class = "error">Please enter the year you were born as
     four digits</p>';
     $okay = false;
+}
+
+//检查出生年是否合法
+if ($_POST['year'] > 2021)
+{
+    print '<p class = "error">Either you entered you birth year wrong or you come from the future!</p>';
+    $okay = false;
+}*/
+
+//验证年
+if (is_numeric($_POST['year']) AND (strlen($_POST['year'] == 4)))
+{
+    //检查出生年是否在2021前
+    if ($_POST['year'] < 2021)
+    {
+        $age = 2021 -$_POST['year'];
+    }
+    else
+    {
+        print '<p class = "error">Either you entered you birth year wrong or you come from the future!</p>';
+        $okay = false;
+    }
+}
+else
+{
+    print '<p class = "error">Please enter the year you were born as
+    four digits</p>';
+    $okay = false;
+}
+
+//检查用户是否选中条款
+if(!isset($_POST['terms']) AND ($_POST['terms'] == 'Yes'))
+{
+    print '<p class = "error">You Need to accept the terms.</p>';
+    $okay=false;
 }
 
 if ($okay)
