e8323f55ce1cb1310f5156e9bf71a3237e49a264
rhli-desktop <rhliwow1@outlook.com>
2021-02-02 19:55:00 +0800

    向数组添加元素

--- ch7/soups1.php
+++ ch7/soups1.php
@@ -14,7 +14,18 @@
         'Wednesday' => 'Vegetarian',
     );
 
-    print "<p>$soups</p>";
+    $count1 = count($soups);
+    print "<p>The soups array originally had $count1 elements.</p>";
+
+    //添加元素
+    $soups['Thursday'] = 'Chicken Noodle';
+    $soups['Friday'] = 'Tomato';
+    $soups['Saturday'] = 'Cream of Broccoli';
+
+    $count2 = count($soups);
+    print "<p>After adding 3 more soups, the array now has $count2 ekenebts.</p>";
+    
+    // print "<p>$soups</p>";
     print_r ($soups);
 ?>
 </body>
