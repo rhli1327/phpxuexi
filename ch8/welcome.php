1e521797a3933543fc3c5550aba85f99fa53b116
rhli-desktop <rhliwow1@outlook.com>
2021-02-04 01:29:55 +0800

    登录成功页面

---
+++ ch8/welcome.php
@@ -0,0 +1,13 @@
+<?php
+    // 用户成功登陆后会被重定向到这里
+
+    // 标题
+    define('TITLE','Welcome to Gay Club!');
+    include ('header.html');
+?>
+
+<h2>Welcome to the Gay Club!</h2>
+<p>You've successfully logged in and can now take advantage of everything the site has to offer.</p>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+<?php include('footer.html'); // Need the footer. ?>
