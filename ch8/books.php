f12872236c5ee076bc42d7f9e1e029cf1aec16f3
rhli-desktop <rhliwow1@outlook.com>
2021-02-03 21:26:42 +0800

    标题使用常量的形式

---
+++ ch8/books.php
@@ -0,0 +1,16 @@
+<?php
+// 这个页面显示傻逼的著作
+    define('TITLE', 'Books by J.D. Salinger');
+    include('header.html');
+?>
+<h2>J.D. Salinger's Books</h2>
+<ul>
+	<li>The Catcher in the Rye</li>
+	<li>Nine Stories</li>
+	<li>Franny and Zooey</li>
+	<li>Raise High the Roof Beam, Carpenters and Seymour: An Introduction</li>
+</ul>
+
+<?php
+    include('footer.html');
+?>
